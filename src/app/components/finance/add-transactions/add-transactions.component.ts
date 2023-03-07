25ccd2f85c4f5b932aa3c38a1553bbcd541aa0bc
Lovekesh Pal <lovekeshfgr@gmail.com>
2023-03-07 23:00:53 +0530

    Making Navbar Responsive

--- src/app/components/finance/add-transactions/add-transactions.component.ts
+++ src/app/components/finance/add-transactions/add-transactions.component.ts
@@ -9,9 +9,6 @@ import {MatDialog} from "@angular/material/dialog";
 })
 export class AddTransactionsComponent {
 
-
-
-
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
 
 })
 export class HeaderComponent{
+navbarOpen: boolean = false;
 
+toggleNavbar(){
+  this.navbarOpen = !this.navbarOpen;
+}
 }
 
 
